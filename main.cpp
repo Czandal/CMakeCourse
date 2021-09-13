52ee93efa170d255395ef8a1e6493cae1afae986
Czandal <malec.radoslaw@o2.pl>
2021-09-13 13:40:17 +0200

    Added Versioning V2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "Machine1conf.h"
 #include "Function.hpp"
-int main()
+int main(int argc, char**argv)
 {
 	std::cout << "Hello CMake!\n";
+	std::cout << argv[0]<<" " << Machine1_VERSION_MAJOR <<" "<< Machine1_VERSION_MINOR <<"\n";
 	std::cout << Returner() << "\n";
 	return 0;
 }
