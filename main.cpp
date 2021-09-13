d78dd6a2f998872e3c3bca5daad8ec2f2acdca6a
Czandal <malec.radoslaw@o2.pl>
2021-09-13 14:49:43 +0200

    Added optional library

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <Machine1conf.h>
-#include "Function.hpp"
+
 int main(int argc, char**argv)
 {
 	std::cout << "Hello CMake!\n";
 	std::cout << argv[0]<<" " << Machine1_VERSION_MAJOR <<" "<< Machine1_VERSION_MINOR <<"\n";
+#ifdef USE_MACHINELIB
 	std::cout << Returner() << "\n";
+#endif // !USE_MACHINELIB
+
 	return 0;
 }
